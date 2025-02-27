23784daad626ddbd9c75d9024bed8777e5c57425
Junpei Ikuta <171251543+Iktahana@users.noreply.github.com>
2025-02-27 22:22:12 +0900

    Add a new endpoint to add cards via POST requests
    Introduce a `/add` endpoint to handle JSON input for adding cards. This leverages the `add_card` function to process card details like `card_number`, `card_name`, associated devices, TTL, and persistence. Ensures easy creation of cards through the API.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
 
-from card import identify_by_sn_card
+from card import identify_by_sn_card, add_card
 
 app = FastAPI()
 
@@ -36,3 +36,17 @@ async def vguang_identify(device_sn: str, request: Request):
         )
 
     raise False
+
+
+@app.post("/add")
+async def identify_json(request: Request):
+    body = await request.json()
+    return {
+        add_card(
+            card_number=body.get("card_number"),
+            card_name=body.get("card_name"),
+            devices=body.get("devices"),
+            ttl=body.get("ttl", 60 * 60 * 24),
+            persist=body.get("persist", False)
+        )
+    }
