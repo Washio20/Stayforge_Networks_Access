5d6aff0309c6d874bf51ac699caf47d37c4ac82e
Junpei Ikuta <171251543+Iktahana@users.noreply.github.com>
2025-03-02 01:18:40 +0900

    Refactor card handling and add dynamic FastAPI endpoints
    Enhanced the `Card` class with Pydantic models, structured methods for card operations, and Redis integration handling. Updated FastAPI endpoints for better card management and identification processes, replacing static endpoints with dynamic ones using environment-based configurations.

--- card.py
+++ card.py
@@ -5,100 +5,162 @@ import hashlib
 import json
 import os
 import uuid
+from datetime import datetime, timezone
+from typing import *
 
 import redis
 from fastapi.logger import logger
+from pydantic import BaseModel, constr
 
 
-def _connect_to_redis(host=os.getenv('REDIS_HOST'), port=os.getenv("REDIS_PORT", 6379), db=0):
-    try:
-        connection = redis.StrictRedis(host=host, port=port, db=db)
-        connection.ping()  # Test the connection
-        logger.info("Successfully connected to Redis!")
-        return connection
-    except redis.ConnectionError as e:
-        test_connection = redis.StrictRedis(host=host, port=port, db=db)
-        if test_connection.ping():
-            logger.error("Redis connection test succeeded, but error occurred in primary connection.")
-        print(f"Redis connection failed: {e}")
-        raise
+class CardModel(BaseModel):
+    number: constr(min_length=8, max_length=128)  # Key
+    name: Optional[str]
+    devices: List[str]
+    ttl: int = 60 * 60 * 24
+    persist: bool = False
+    created_at: Optional[datetime] = datetime.now(tz=timezone.utc)
+    owner_client_id: Optional[str] = None
 
 
-def add_card(
-        card_number=hashlib.sha256(str(uuid.uuid4()).encode()).hexdigest(),
-        card_name=None,
-        devices: list = None,
-        ttl=60 * 60 * 24,
-        persist: bool = False
-) -> (str, dict):
-    """
-    Adds a card to the Redis database, storing associated information such as the
-    name, associated devices, and optionally setting a time-to-live (TTL) or persisting
-    the key indefinitely.
+class Card:
+    def __init__(self, environment='standard'):
+        def _connect_to_redis(host=os.getenv('REDIS_HOST'), port=os.getenv("REDIS_PORT", 6379), db=0):
+            try:
+                connection = redis.StrictRedis(host=host, port=port, db=db)
+                connection.ping()  # Test the connection
+                logger.info("Successfully connected to Redis!")
+                return connection
+            except redis.ConnectionError as e:
+                test_connection = redis.StrictRedis(host=host, port=port, db=db)
+                if test_connection.ping():
+                    logger.error("Redis connection test succeeded, but error occurred in primary connection.")
+                print(f"Redis connection failed: {e}")
+                raise
 
-    This function generates a default card number if none is provided. The card name
-    defaults to a lowercase value of the card number if it is not specified. Devices
-    can optionally be listed and stored along with the card in the database. The key
-    can have a TTL expressed in seconds or persist without expiry if specified.
+        self.environment = environment
+        self.environment_config = {
+            'STANDARD': 0,
+            'SANDBOX': 1
+        }
+        if self.environment.upper() in self.environment_config:
+            self.db = self.environment_config[self.environment]
+        else:
+            raise EnvironmentError(f"Invalid environment: {self.environment}")
 
-    :param card_number: The unique identifier for the card. Defaults to a SHA256
-        hash of a newly generated UUID if not provided.
-    :param card_name: The human-readable name of the card. If not specified, it
-        defaults to the lowercase version of 'card_number'.
-    :param devices: A list of device identifiers associated with this card.
-    :param ttl: The time-to-live for the card in the database, in seconds. Defaults
-        to 24 hours (60 * 60 * 24). If set to 0 or `None`, the card will not expire
-        unless 'persist' is set to True.
-    :param persist: A flag indicating whether the card should be persisted
-        indefinitely in the database regardless of TTL value. Defaults to False.
+        self.redis = _connect_to_redis(
+            db=self.db
+        )
 
-    :return: A tuple containing:
-        - The card number as a string.
-        - A dictionary representation of the card details, including associated
-          devices and the card name.
-        - The remaining TTL in milliseconds or -1 if the card does not expire.
-    """
-    card_name = card_name if card_name else card_number.lower()
-    devices = devices if devices else []
+    def add_card(
+            self,
+            card: CardModel = CardModel(
+                number=hashlib.sha256(str(uuid.uuid4()).encode()).hexdigest(),
+                name=None,
+                devices=[],
+                ttl=60 * 60 * 24,
+                persist=False
+            )
+    ) -> CardModel:
+        """
+        Adds a new card to the system or updates an existing one based on the update_mode value.
 
-    r = _connect_to_redis()
-    r.set(
-        card_number, json.dumps({
-            "name": card_name,
-            "devices": devices,
-        })
-    )
+        If the update_mode is set to False and a card with the same number already exists,
+        a ValueError will be raised. Otherwise, the function stores the card in the Redis
+        database. Optionally, the TTL (time-to-live) and persistence of the card can be
+        configured.
 
-    if ttl:
-        r.pexpire(card_number, ttl * 1000)
+        :param card: The card details to be added or updated. Defaults to a new dynamically
+            created CardModel with a unique number, no name, empty devices list,
+            24-hour TTL, and no persistence.
+        :type card: CardModel
 
-    if persist:
-        r.persist(card_number)
+        :return: The CardModel object representing the added or updated card.
+        :rtype: CardModel
+        :raises ValueError: If `update_mode` is False and a card with the same
+            number already exists in the database.
+        """
+        card.number = card.number.upper()
 
-    return card_number, json.loads(r.get(card_number)), int(r.ttl(card_number))
+        # If card exists
+        if self.redis.exists(card.number):
+            raise ValueError(f"Card with number {card.number} already exists.")
+
+        card.name = card.name if card.name else card.number.upper()
+
+        r = self.redis
+        r.set(
+            card.number.upper(), card.model_dump_json()
+        )
+
+        if card.ttl:
+            r.pexpire(card.number, card.ttl * 1000)
+
+        if card.persist:
+            r.persist(card.number)
+
+        return self.get_a_card(card.number)
+
+    def get_a_card(self, card_number):
+        """
+        Retrieves and constructs a card object using the card number by fetching details
+        from a Redis datastore. This function retrieves the data corresponding to the
+        given card number and parses it to construct a CardModel instance containing
+        the card details.
+
+        :param card_number: The card number used to fetch the card details
+                           from the Redis datastore
+        :type card_number: str
+        :return: A CardModel object instantiated with the card details
+        :rtype: CardModel
+        """
+        r = self.redis
+        card_number = card_number.upper()
+
+        if r.exists(card_number):
+            return CardModel(
+                number=card_number,
+                name=json.loads(r.get(card_number))['name'],
+                devices=json.loads(r.get(card_number))['devices'],
+                ttl=r.ttl(card_number),
+                persist=json.loads(r.get(card_number))['persist']
+            )
+        else:
+            raise ValueError(f"Card with number {card_number} not found.")
+
+    def delete_a_card(self, card_number):
+        r = self.redis
+        card_number = card_number.upper()
+        return r.delete(card_number)
+
+    def identify_by_sn_card(
+            self,
+            device_sn, card_number
+    ):
+        """
+        Identifies whether a given device serial number is associated with a specific card
+        number by checking the data stored in the Redis database. The function retrieves
+        data associated with the card number from the Redis database. If such a record is
+        found, it deserializes the stored JSON data and checks if the given device serial
+        number is among the associated devices.
+
+        :param device_sn: The serial number of the device to be checked.
+        :type device_sn: str
+        :param card_number: The card number serving as the key for the Redis database query.
+        :type card_number: str
+        :return: A boolean value indicating whether the device serial number is associated
+                 with the card number.
+        :rtype: bool
+        """
+        r = self.redis
+        card_number = card_number.upper()
+
+        if not r.exists(card_number):
+            raise ValueError(f"Card with number {card_number} not found.")
+
+        data = json.loads(r.get(card_number))
+        return device_sn in data['devices']
 
 
-def identify_by_sn_card(device_sn, card_number):
-    """
-    Identify if a device serial number (device_sn) is associated with a card number.
-
-    This function retrieves data associated with a given card number from a Redis
-    database and determines if a specified device serial number (device_sn) is
-    registered to that card. The function leverages JSON parsing to process the
-    data stored in Redis and checks for membership of the device_sn in the
-    associated list of devices.
-
-
-    :param device_sn: The serial number of the device to check in association
-        with the card number.
-    :type device_sn: str
-        :param card_number: The card number to be looked up in the Redis database.
-    :type card_number: str
-    :return: A boolean value indicating whether the given device serial number is
-        associated with the provided card number.
-    :rtype: bool
-    """
-    r = _connect_to_redis()
-    data = json.loads(r.get(card_number))
-
-    return device_sn in data['devices']
+class CardIdentifyResponse(CardModel):
+    message: str
--- main.py
+++ main.py
@@ -1,52 +1,95 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import PlainTextResponse
+import uuid
 
-from card import identify_by_sn_card, add_card
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import PlainTextResponse
+from uvicorn.config import logger
+
+from card import Card, CardIdentifyResponse, CardModel
 
 app = FastAPI()
 
 
-@app.post("/identify/json/{device_sn}")
-async def identify_json(device_sn: str, request: Request):
+@app.get("/")
+async def healthcheck():
+    try:
+        Card()
+    except Exception as e:
+        tracer_code = str(uuid.uuid4()).replace("-", "")
+        logger.error(f"ERROR TRACER CODE '{tracer_code}': {e}")
+        return PlainTextResponse(f"HTTP 500 ERROR ({tracer_code})", status_code=500)
+
+    return PlainTextResponse("ok")
+
+
+@app.post("/card")
+async def get_a_card_information(request: Request):
+    json = await request.json()
+    return Card().get_a_card(json.get("card_number"))
+
+
+@app.post("/card/add", response_model=CardModel, tags=["card", "add"])
+async def create_a_card(request: Request):
     body = await request.json()
-    card_number = body.get("card_number")
+    body["number"] = body["number"].upper()
 
-    if identify_by_sn_card(device_sn, card_number):
-        return True
+    card_obj = Card(
+        environment=request.headers.get("X-Environment", "standard").upper(),
+    )
 
-    raise False
+    try:
+        return card_obj.add_card(
+            CardModel(
+                **body
+            )
+        )
+    except ValueError as e:
+        raise HTTPException(
+            status_code=400,
+            detail={"message": str(e)}
+        )
 
 
-@app.post("/identify/card_number_text/{device_sn}")
-async def identify_json(device_sn: str, request: Request):
-    body = await request.json()
-    card_number = body.get("card_number")
+@app.post("/identify/json", response_model=CardIdentifyResponse)
+@app.post("/identify/json/{device_sn}", response_model=CardIdentifyResponse)
+async def identify_json(request: Request, device_sn: str = None):
+    device_sn = device_sn or request.headers.get("X-Device-SN")
+    json_data = await request.json()
+    card_number = json_data.get("card_number").upper()
 
-    return identify_by_sn_card(device_sn, card_number)
+    card_obj = Card(environment=request.headers.get("X-Environment", "standard").upper())
+    client_id = None
+
+    print(device_sn, card_number)
+
+    # Verify that the card owner or client_id is empty
+    if client_id is not None and card_obj.get_a_card(card_number).owner_client_id != client_id:
+        raise HTTPException(status_code=400, detail={
+            "message": "Unable to be identify because owner is not true. Please check your token."
+        })
+
+    # Verify the card number paired the device SN
+    if card_obj.identify_by_sn_card(device_sn, card_number):
+        return CardIdentifyResponse(message="Successfully", **card_obj.get_a_card(card_number).model_dump())
+
+    raise HTTPException(status_code=400, detail={
+        "message": "Unable to be identify successfully."
+    })
 
 
-@app.post("/identify/vguang/{device_sn}")
+@app.post("/identify/vguang-m350/{device_sn}")
 async def vguang_identify(device_sn: str, request: Request):
-    body = await request.json()
-    card_number = body.get("card_number")
+    raw_body = await request.body()
+    text_content = raw_body.decode("utf-8")
+    card_number = text_content.upper()
 
-    if identify_by_sn_card(device_sn, card_number):
-        return PlainTextResponse(
-            "code=0000"
-        )
+    print(device_sn, card_number)
 
-    raise False
+    card_obj = Card(environment="standard".upper())
 
+    # Verify the card number paired the device SN
+    if card_obj.identify_by_sn_card(device_sn, card_number):
+        return PlainTextResponse("code=0000")
 
-@app.post("/add")
-async def identify_json(request: Request):
-    body = await request.json()
-    return {
-        'result': add_card(
-            card_number=body.get("card_number"),
-            card_name=body.get("card_name"),
-            devices=body.get("devices"),
-            ttl=body.get("ttl", 60 * 60 * 24),
-            persist=body.get("persist", False)
-        )
-    }
+    raise HTTPException(status_code=400, detail={
+        "message": "Unable to be identify successfully."
+    })
